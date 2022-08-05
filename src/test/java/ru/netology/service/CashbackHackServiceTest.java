fad384e159033c1a6700801dc144a1b5e77d8247
MaksDim <demasig@yandex.ru>
2022-08-05 18:13:43 +0400

    correct

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -19,8 +19,8 @@ class CashbackHackServiceTest {
     @Test
     void schould1000IfAmountIs0() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 0;
-        int expected = 1000;
+        int amount = 1000;
+        int expected = 0;
 
         int actual = service.remain(amount);
         assertEquals(expected, actual);
